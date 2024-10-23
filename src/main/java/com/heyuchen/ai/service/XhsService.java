85f278b9331a4669f6735906189525a98cd7e429
chenmutime <yuchentime@gmail.com>
2024-10-23 23:01:27 +0800

    移除无用代码

--- src/main/java/com/heyuchen/ai/service/XhsService.java
+++ src/main/java/com/heyuchen/ai/service/XhsService.java
@@ -1,24 +1,31 @@
 package com.heyuchen.ai.service;
 
-import com.heyuchen.ai.constants.ModelProviderEnum;
-import com.heyuchen.ai.factory.ChatClientFactory;
+import com.heyuchen.ai.controller.XhsController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
-import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.zhipuai.ZhiPuAiChatModel;
+import org.springframework.ai.zhipuai.ZhiPuAiChatOptions;
+import org.springframework.ai.zhipuai.api.ZhiPuAiApi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
 import java.util.Map;
 
 @Service
 public class XhsService {
 
-    private final ChatClientFactory chatClientFactory;
+    private static final Logger logger = LogManager.getLogger(XhsService.class);
 
-    public XhsService(ChatClientFactory chatClientFactory) {
-        this.chatClientFactory = chatClientFactory;
-    }
+    @Autowired
+    private ZhiPuAiChatModel zhipuChatModel;
 
     public JSONObject rate(String text, String keyword, String positive, String negative) {
-        ChatClient chatClient = chatClientFactory.getChatClient(ModelProviderEnum.ZHIPU);
         var template = """
                 ## 任务描述
                 我将会提供给你一段<文本>，请你根据该段文本的<主题>判断其是否满足<评估要求>，然后给出评分，评分范围1～10。
@@ -39,9 +46,21 @@ public class XhsService {
                 ```
                 """;
         PromptTemplate promptTemplate = new PromptTemplate(template, Map.of("text", text, "keyword", keyword, "positive", positive, "negative", negative));
-        String output = chatClient.prompt(promptTemplate.create()).call().content();
-        System.out.println(output);
-        return parseOutput(output);
+        String modelName = StringUtils.hasText(System.getenv("zhipuai.model")) ? System.getenv("zhipuai.model") : ZhiPuAiApi.ChatModel.GLM_4_AirX.getValue();
+        logger.info("current model name is: {}", modelName);
+        ChatResponse chatResponse = zhipuChatModel.call(
+                new Prompt(
+                        promptTemplate.create().getContents(),
+                        ZhiPuAiChatOptions.builder()
+                                .withModel(modelName)
+                                .withTemperature(0.5)
+                                .build()
+                ));
+        if (CollectionUtils.isEmpty(chatResponse.getResults())) {
+            return new JSONObject("{}");
+        }
+//        System.out.println(output);
+        return parseOutput(chatResponse.getResult().getOutput().getContent());
     }
 
     private JSONObject parseOutput(String output) {
